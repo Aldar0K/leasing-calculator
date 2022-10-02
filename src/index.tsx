2dcfcaf1278639db1d3edbd56ae71b25448ecfa7
Aldar <aldarkonov@mail.ru>
2022-10-02 17:50:17 +0300

    feat: add sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/reset.css';
-import './styles/global.css';
+import './styles/style.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
