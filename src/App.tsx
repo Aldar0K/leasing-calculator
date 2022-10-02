6939f137df277cc0ed2fad560d7ab9f07ae00e48
Aldar <aldarkonov@mail.ru>
2022-10-02 17:57:27 +0300

    feat: add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="wrapper">
-      <main className="main"></main>
+      <main className="main">
+        <h1>Рассчитайте стоимость автомобиля в лизинг</h1>
+      </main>
     </div>
   );
 }
